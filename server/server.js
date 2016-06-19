633138ce0da281a6880e0698622e7fffa566dc7d
Andrew <lisowski54@gmail.com>
2016-06-19 14:10:12 -0700

    oops https

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ Meteor.startup(function () {
   ServiceConfiguration.configurations.remove({ service: 'soundCloud' });
   ServiceConfiguration.configurations.remove({ service: 'soundcloud' });
   console.log(Meteor.absoluteUrl())
-  if (Meteor.absoluteUrl() == 'http://sctunes.herokuapp.com/') {
+  if (Meteor.absoluteUrl() == 'https://sctunes.herokuapp.com/') {
     config = {
       clientId: '51c5ebff845639af50314b134ae1e904',
       loginStyle: 'popup',
