fc14e16210b0f4053cdbda277e164b206398df2e
Andrew <lisowski54@gmail.com>
2016-06-23 00:23:06 -0700

    better description

--- client/components/ArtistInfo/artistInfo.js
+++ client/components/ArtistInfo/artistInfo.js
@@ -4,14 +4,7 @@ import { Router } from 'meteor/iron:router';
 
 Template.artistInfo.helpers({
   artistData: () => Session.get('currentArtist'), // eslint-disable-line meteor/no-session
-  description() {
-    const text = Session.get('currentArtist').description; // eslint-disable-line meteor/no-session
-
-    if (text)
-      return text.trim();
-
-    return text;
-  },
+  trim: text => text.trim(),
   isActive(name) {
     if (name === Router.current().route.getName() ||
       name === 'artistTracks' && Router.current().route.getName() === 'artist')
